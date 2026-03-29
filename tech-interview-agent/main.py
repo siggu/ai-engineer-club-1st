44224842ffa4125ee6b20334ace3160540ca142c
siggu <siggu100@gmail.com>
2026-03-29 22:46:28 +0900

    feat: tool 설정
    - tavily 패키지 추가
    - tool 노드 설정

--- tech-interview-agent/main.py
+++ tech-interview-agent/main.py
@@ -1,10 +1,13 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages, MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
 from typing_extensions import TypedDict
 from typing import Annotated
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import AnyMessage
+from langchain_core.tools import tool
 from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
@@ -17,11 +20,26 @@ class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
 
 
-# 챗봇 노드 설정
+# Tool 설정
+web_search_tool = TavilySearch(
+    max_results=3,
+    description=("웹 서치 기능이 필요할 때 사용합니다."),
+)
+
+
+llm_with_tools = llm.bind_tools(tools=[web_search_tool])
+
+
+# 챗봇 노드 생성
 def chatbot(state: State) -> State:
-    response = llm.invoke(state["messages"])
+    response = llm_with_tools.invoke(state["messages"])
     return {"messages": [response]}
 
+
+# tool 노드 생성
+tool_node = ToolNode(
+    tools=[web_search_tool],
+)
 
 # StateGraph 설정
 graph_builder = StateGraph(State)
@@ -29,14 +47,18 @@ graph_builder = StateGraph(State)
 
 # 노드 설정
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
 
 
 # 엣지 설정
 graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+graph_builder.add_edge("tools", "chatbot")
 
 
 # 그래프 컴파일
 graph = graph_builder.compile()
 
-print(graph.invoke({"messages": [{"role": "user", "content": "안녕하세요!"}]}))
+print(
+    graph.invoke({"messages": [{"role": "user", "content": "현재 환율은 얼마인가요"}]})
+)
